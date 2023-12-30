4a0592ee5a78cefc8639dccd266199bf2d2364a4
VIKALP <113850666+vikalp026var@users.noreply.github.com>
2023-12-30 23:14:12 +0530

    first commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup,find_packages
+
+setup(
+     name='House Price Prediction',
+     author='Vikalp',
+     version='0.0.1',
+     author_email='vikalp026varshney@gmail.com',
+     packages=find_packages(),
+     include_dirs=[],
+
+)
